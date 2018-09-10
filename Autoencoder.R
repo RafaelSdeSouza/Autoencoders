7a7df7ea9d61487fff3ead0d3aa41aeaa855835d
Rafael S. de Souza <rafael.2706@gmail.com>
2018-09-10 10:33:52 -0400

    e

--- Autoencoder.R
+++ Autoencoder.R
@@ -1,13 +1,19 @@
 library(h2o)
 h2o.init(nthreads = -1)
+require(imager)
+library(rvest)
+search <- read_html("https://www.google.com/search?site=&tbm=isch&q=parrot")
 
-index <- sample(seq(1:nrow(iris)),0.8*nrow(iris),replace=F)
+#Grab all <img> tags, get their "src" attribute, a URL to an image
+urls <- search %>% html_nodes("img") %>% html_attr("src") #Get urls of parrot pictures
 
+#Load the first four, return as image list, display
+map_il(urls[1],load.image) %>% plot
 
 iris <- as.h2o(iris)
-parts <- h2o.splitFrame(iris,0.8)
-train <- parts[[1]]
-test <- parts[[2]]
+#parts <- h2o.splitFrame(iris,1)
+train <- iris
+#test <- parts[[2]]
 
 m_AE_4 = h2o.deeplearning(x = c("Sepal.Length", "Sepal.Width","Petal.Length",
                                 "Petal.Width"),
@@ -16,7 +22,7 @@ m_AE_4 = h2o.deeplearning(x = c("Sepal.Length", "Sepal.Width","Petal.Length",
                           epochs = 100,
                           model_id = "m_AE_4",
                           train_samples_per_iteration=nrow(train),
-                          hidden = c(4,3,4),
+                          hidden = c(4,2,4),
                           activation="Tanh"
                           
 )
@@ -24,8 +30,11 @@ m_AE_4 = h2o.deeplearning(x = c("Sepal.Length", "Sepal.Width","Petal.Length",
 sh = h2o.scoreHistory(m_AE_4)
 plot(as.data.frame(h2o.scoreHistory(m_AE_4))$training_mse,type="l")
 
-train_1 <- h2o.deepfeatures(m_AE_4,train,3)
-plot(as.data.frame(train_1))
+train_1 <- as.data.frame(h2o.deepfeatures(m_AE_4,train,2))
+
+gd <- data.frame(x=train_1[,1], y=train_1[,2],col=as.data.frame(iris[,5]))
+ggplot(gd,aes(x=x,y=y,colour=Species))+
+  geom_point()
 
 d1 <- as.data.frame(train_1)$DF.L2.C1
 dens  <- densityMclust(d1)
--- Cepheids/Autoencoder.R
+++ Cepheids/Autoencoder.R
@@ -1,17 +1,29 @@
-
-
-
-
-tmp <- load("installed_old.rda")
-
-install.packages(installedpkgs,dependencies = T)
-
-
-
-install.packages("h2o")
-
 library(h2o)
 h2o.init()
+
+
+
+
+m_AE_4 = h2o.deeplearning(x = c("x", "y"),
+                          training_frame=cc,
+                          autoencoder=TRUE,
+                          epochs = 500,
+                          score_each_iteration=T,
+                          model_id = "m_AE_4",
+                          train_samples_per_iteration=nrow(cc),
+                          activation = "Tanh",
+                          hidden = c(2,1,2)
+                          
+)
+
+sh = h2o.scoreHistory(m_AE_4)
+plot(as.data.frame(h2o.scoreHistory(m_AE_4))$training_mse,type="l")
+
+train_1 <- h2o.deepfeatures(m_AE_4,cc,layer=3)
+
+d1 <- as.data.frame(train_1)
+plot(d1)
+
 
 
 
@@ -19,8 +31,8 @@ h2o.init()
 Lyrae <- read.table("blg_met_rrl.dat",header=T) %>% mutate(.,Period = log10(Period)) %>%
   filter(X.Fe.H. > -1.647874 && X.Fe.H. < -0.279256)
 
-test_index <- sample(seq_len(nrow(Lyrae)),replace=F, size = 5000)
-Lyrae_short <- Lyrae[test_index,c("Period","X.Fe.H.","R21")] 
+test_index <- sample(seq_len(nrow(Lyrae)),replace=F, size = 20000)
+Lyrae_short <- Lyrae[test_index,c("Period","X.Fe.H.","R21","Imag")] 
 
 
 LyraeH20  <- as.h2o(Lyrae)
@@ -29,25 +41,27 @@ parts <- h2o.splitFrame(LyraeH20,0.8)
 train <- parts[[1]]
 test <- parts[[2]]
 
-m_AE_4 = h2o.deeplearning(x = c("Period", "R21","X.Fe.H."),
+m_AE_4 = h2o.deeplearning(x = c("Period", "R21","X.Fe.H.","Imag"),
                           training_frame=train,
                           autoencoder=TRUE,
-                          epochs = 100,
+                          epochs = 500,
+                          score_each_iteration=T,
                           model_id = "m_AE_4",
                           train_samples_per_iteration=nrow(train),
-                          hidden = c(3,2,1,2,3),
-                          activation="Tanh"
+                          activation = "Tanh",
+                          hidden = c(50,25,2,25,50)
                           
 )
 
 sh = h2o.scoreHistory(m_AE_4)
 plot(as.data.frame(h2o.scoreHistory(m_AE_4))$training_mse,type="l")
 
-train_1 <- h2o.deepfeatures(m_AE_4,train,3)
+train_1 <- h2o.deepfeatures(m_AE_4,train,layer=3)
 
 
-d1 <- as.data.frame(train_1)$DF.L2.C1
-dens  <- densityMclust(d1)
+
+d1 <- as.data.frame(train_1)
+dens  <- Mclust(train_1)
 
 summary(dens)
 plot(dens, what = "density", data = d1)
